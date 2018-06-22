675e311ec56f0f079c8a6fa11cee0a1a5a17eda9
xlaoy <768881412@qq.com>
2018-06-22 11:17:54 +0800

    应用

--- src/main/java/com/xlaoy/discovery/DiscoveryApplication.java
+++ src/main/java/com/xlaoy/discovery/DiscoveryApplication.java
@@ -1,9 +1,11 @@
 package com.xlaoy.discovery;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableSwagger2Doc
 @EnableEurekaServer
 @SpringCloudApplication
 public class DiscoveryApplication {
